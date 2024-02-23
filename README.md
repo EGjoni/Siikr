24fbc330daf4dfb9d932a7f653649847345fb421
Alek Ratzloff <alekratz@gmail.com>
2024-02-23 10:51:06 -0800

    docker: Rename siikr.conf -> siikr.example.env
    Since siikr.conf is more of an environment file, the extension should
    reflect that. I have also renamed it to siikr.example.env - this is so
    that if the configuation is changed, it will only go into the example
    env file, and not cause issues with pulling down into a modified
    siikr.env file. Additionally, it helps prevent in-dev configuration from
    accidentally getting committed in the case that someone uses
    `git commit -a` a bit carelessly.
    
    Signed-off-by: Alek Ratzloff <alekratz@gmail.com>

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ IYKYK.
 Best done on a clean Ubuntu or debian server.
 
 0. clone this repo.
-1. Fill out (at a minimum) the first three lines in `siikr.conf`
+1. Copy `siikr.example.env` to `siikr.env` and fill out (at a minimum) the first three lines.
 2. If not already in a terminal, switch to one and run `chmod +x setup_simple.sh`
 3. Then run `sudo ./setup_simple.sh` and follow the prompts.
 4. Pray.
--- setup_simple.sh
+++ setup_simple.sh
@@ -4,8 +4,12 @@ set -eo pipefail
 script_path="$(realpath "$0")"
 script_dir="$(dirname "$script_path")"
 
-source "$script_dir/siikr.conf"
+if [[ ! -f "$script_dir/siikr.env" ]]; then
+    echo "Could not find $script_dir/siikr.env. Make sure to copy siikr.example.env and fill it out with your deploy information."
+    exit 1
+fi
 
+source "$script_dir/siikr.env"
 
 update_install_packages() {
     local confirm
