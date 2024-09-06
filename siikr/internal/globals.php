95e02b0204c607460fea9344e1b2ca42603bc237
EGjoni <rufsketch1@gmail.com>
2024-09-06 16:56:02 +0000

    let's avoid dependencies

--- siikr/internal/globals.php
+++ siikr/internal/globals.php
@@ -823,3 +823,16 @@ function binarySearchMissing($db, $blog_info) {
     );
     return null;
 }
+
+
+
+function uuid_create_v4() {
+    return sprintf(
+        '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+        mt_rand(0, 0xffff),
+        mt_rand(0, 0x0fff) | 0x4000,
+        mt_rand(0, 0x3fff) | 0x8000,
+        mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+    );
+}
